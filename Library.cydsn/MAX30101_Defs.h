5c100cc0035907b4107c9450e6bc318b872cd719
Davide Marzorati <davide4.marzorati@mail.polimi.it>
2020-11-30 12:12:05 +0100

    Added documentation for I2C registers

--- Library.cydsn/MAX30101_Defs.h
+++ Library.cydsn/MAX30101_Defs.h
@@ -33,7 +33,7 @@
     #define MAX30101_ERROR 2
     
     //==============================================
-    //           MAX30101 I2C REGISTERS
+    //     MAX30101 INTERRUPT STATUTS I2C REGISTERS
     //==============================================
     
     /**
@@ -77,6 +77,10 @@
     /**
     *   \brief Interrupt Status 2 register.
     *
+    *   Whenever an interrupt is triggered, the MAX30101 pulls the active-low
+    *   interrupt pin into its low state until the interrupt is cleared. This
+    *   register contains the status of the interrupts.
+    
     *   The register is structured as follows:
     *   |   B7   |   B6   |    B5  |   B4   |   B3   |   B2   |   B1   |   B0   |
     *   | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | 
@@ -90,19 +94,221 @@
     */
     #define MAX30101_INT_ST_2 0x01
     
+    //==============================================
+    //   MAX30101 INTERRUPT ENABLE I2C REGISTERS
+    //==============================================
     /**
     *   \brief Interrupt Enable 1 register.
     *
+    *   Each source of hardware interrupt, with the exception of power ready, can be disabled in a 
+    *   software register within the MAX30101 IC. The power-ready interrupt cannot be disabled 
+    *   because the digital state of the module is reset upon a brownout condition (low power supply voltage),
+    *   and the default condition is that all the interrupts are disabled. 
+    *   Also, it is important for the system to know that a brownout condition has occurred, 
+    *   and the data within the module is reset as a result.
+    *   The unused bits should always be set to zero for normal operation.
+    *
+    *   The register is structured as follows:
+    *   |   B7   |   B6   |    B5  |   B4   |   B3   |   B2   |   B1   |   B0   |
+    *   | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | 
+    *   | A_FULL_EN | PPG_RDY_EN | ALC_OVF_EN | | | | | |
     *
     */
-    #define MAX30101_INT_EN_1
+    #define MAX30101_INT_EN_1 0x02
     
     /**
     *   \brief Interrupt Enable 2 register.
     *
+    *   Each source of hardware interrupt, with the exception of power ready, can be disabled in a 
+    *   software register within the MAX30101 IC. The power-ready interrupt cannot be disabled 
+    *   because the digital state of the module is reset upon a brownout condition (low power supply voltage),
+    *   and the default condition is that all the interrupts are disabled. 
+    *   Also, it is important for the system to know that a brownout condition has occurred, 
+    *   and the data within the module is reset as a result.
+    *   The unused bits should always be set to zero for normal operation.
     *
+    *   The register is structured as follows:
+    *   |   B7   |   B6   |    B5  |   B4   |   B3   |   B2   |   B1   |   B0   |
+    *   | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | 
+    *   |        |         |         | | | | DIE_TMP_RDY_EN | |
     */
-    #define MAX30101_INT_EN_2
+    #define MAX30101_INT_EN_2 0x03
+    
+    //==============================================
+    //        MAX30101 FIFO I2C REGISTERS
+    //==============================================
+    /**
+    *   \brief FIFO write pointer register.
+    *
+    *   The FIFO Write Pointer points to the location where the MAX30101 writes the next sample. 
+    *   This pointer advances for each sample pushed on to the FIFO.
+    *   It can also be changed through the I2C interface when MODE[2:0] in #MAX30101_MODE_CONF is 010, 011, or 111.
+    *
+    *   The register is structured as follows:
+    *   |   B7   |   B6   |    B5  |   B4   |   B3   |   B2   |   B1   |   B0   |
+    *   | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | 
+    *   |        |        |        |       <td colspan=5>FIFO_WR_PTR[4:0]       |
+    */
+    #define MAX30101_FIFO_WP 0x04
+    
+    /**
+    *   \brief FIFO overflow counter register.
+    *
+    *   When the FIFO is full, samples are not pushed on to the FIFO, samples are lost. 
+    *   OVF_COUNTER counts the number of samples lost. It saturates at 0x1F.
+    *   When a complete sample is “popped” (i.e., removal of old FIFO data and shifting the samples down) 
+    *   from the FIFO (when the read pointer advances), OVF_COUNTER is reset to zero.
+    *
+    *   The register is structured as follows:
+    *   |   B7   |   B6   |    B5  |   B4   |   B3   |   B2   |   B1   |   B0   |
+    *   | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | 
+    *   |        |        |        |       <td colspan=5>OVF_COUNTER[4:0]       |
+    */
+    #define MAX30101_FIFO_OVF_CNT 0x05
+    
+    /**
+    *   \brief FIFO read pointer register.
+    *
+    *   The FIFO Read Pointer points to the location from where the processor gets the next sample
+    *   from the FIFO through the I2C interface. This advances each time a sample is popped from
+    *   the FIFO. The processor can also write to this pointer after reading the samples 
+    *   to allow rereading samples from the FIFO if there is a data communication error.
+    *
+    *   The register is structured as follows:
+    *   |   B7   |   B6   |    B5  |   B4   |   B3   |   B2   |   B1   |   B0   |
+    *   | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | 
+    *   |        |        |        |       <td colspan=5>FIFO_RD_PTR[4:0]       |
+    */
+    #define MAX30101_FIFO_RP 0x06
+    
+    /**
+    *   \brief FIFO data register.
+    *
+    *   The circular FIFO depth is 32 and can hold up to 32 samples of data.
+    *   The sample size depends on the number of LED channels (a.k.a. channels) configured as active. 
+    *   As each channel signal is stored as a 3-byte data signal, the FIFO width can be 3 bytes, 6 bytes, 9 bytes, 
+    *   or 12 bytes in size. The #MAX30101_FIFO_DATA register in the I2C register map points to the next sample to be read 
+    *   from the FIFO. #MAX30101_FIFO_RD_PTR points to this sample. Reading #MAX30101_FIFO_DATA register, 
+    *   does not automatically increment the I2C register address. Burst reading this register, 
+    *   reads the same address over and over.
+    *   Each sample is 3 bytes of data per channel (i.e., 3 bytes for RED, 3 bytes for IR, etc.). 
+    *   The FIFO registers (0x04–0x07) can all be written and read, but in practice only the #MAX30101_FIFO_RD_PTR
+    *   register should be written to in operation. The others are automatically incremented or filled with data 
+    *   by the MAX30101. When starting a new SpO2 or heart rate conversion, it is recommended to first clear 
+    *   the #MAX30101_FIFO_WR_PTR, #MAX30101_OVF_COUNTER, and #MAX30101_FIFO_RD_PTR registers to all zeroes (0x00) 
+    *   to ensure the FIFO is empty and in a known state. When reading the MAX30101 registers in one
+    *   burst-read I2C transaction, the register address pointer typically increments so that the next byte of
+    *   data sent is from the next register, etc. The exception to this is the #MAX30101_FIFO_DATA data register,
+    *   register 0x07. When reading this register, the address pointer does not increment, but the #MAX30101_FIFO_RD_PTR does.
+    *   So the next byte of data sent represents the next byte of data available in the FIFO.
+    *
+    *   The register is structured as follows:
+    *   |   B7   |   B6   |    B5  |   B4   |   B3   |   B2   |   B1   |   B0   |
+    *   | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | 
+    *   |                  <td colspan=8>FIFO_DATA[7:0]                         |
+    */
+    #define MAX30101_FIFO_DATA 0x07
+    
+    //==============================================
+    //   MAX30101 CONFIGURATION I2C REGISTERS
+    //==============================================
+    
+    /**
+    *   \brief FIFO Configuration register.
+    *   
+    *   This register allows to configure the FIFO behaviour.
+    
+    *   It is structured as follows:
+    *   |   B7   |   B6   |    B5  |   B4   |   B3   |   B2   |   B1   |   B0   |
+    *   | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | 
+    *   | <td colspan=3>SMP_AVG[2:0] | FIFO_ROL_LOVER_EN | FIFO_A_FULL[3:0] |
+    *
+    *   **Bits 7:5: Sample Averaging (SMP_AVE)**
+    *
+    *   To reduce the amount of data throughput, adjacent samples (in each individual channel)
+    *   can be averaged and decimated on the chip by setting this register.
+    *
+    *   | SMP_AVG[2:0] | NO. OF SAMPLES AVG PER SAMPLE |
+    *   |:------------:|:-----------------------------:|
+    *   |   000        |  1 (no averaging)             |
+    *   |   001        |                 2             |
+    *   |   010        |                 4             |
+    *   |   011        |                 8             |
+    *   |   100        |                16             |
+    *   |   101        |                32             |
+    *   |   110        |                32             |
+    *   |   111        |                32             |
+    *
+    *   **Bit 4: FIFO Rolls on Full (FIFO_ROLLOVER_EN)**
+    *
+    *   This bit controls the behavior of the FIFO when the FIFO becomes
+    *   completely filled with data. If FIFO_ROLLOVER_EN is set (1), 
+    *   the FIFO Address rolls over to zero and the FIFO continues to fill 
+    *   with new data. If the bit is not set (0), then the FIFO is not updated 
+    *   until FIFO_DATA is read or the WRITE/READ pointer positions are changed.
+    *   
+    *   **Bits 3:0: FIFO Almost Full Value (FIFO_A_FULL) **
+    *   
+    *   This register sets the number of data samples (3 bytes/sample) remaining in the FIFO when the interrupt is issued. 
+    *   For example, if this field is set to 0x0, the interrupt is issued when there is 0 data samples remaining in the 
+    *   FIFO (all 32 FIFO words have unread data). Furthermore, if this field is set to 0xF, the interrupt is issued when
+    *   15 data samples are remaining in the FIFO (17 FIFO data samples have unread data).
+    */
+    #define MAX30101_FIFO_CONF 0x08
+    
+    /**
+    *   \brief MAX30101 Mode COnfiguration.
+    *
+    *   This register is structured as follows:
+    *
+    *   |   B7   |   B6   |    B5  |   B4   |   B3   |   B2   |   B1   |   B0   |
+    *   | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | 
+    *   | SHDN | RESET | | | | MODE[2:0] |    
+    *
+    *   ** Bit 7: Shutdown Control (SHDN) **
+    *
+    *   The part can be put into a power-save mode by setting this bit to one. 
+    *   While in power-save mode, all registers retain their values, and write/read operations function as normal.
+    *   All interrupts are cleared to zero in this mode.
+    *
+    *   **Bit 6: Reset Control (RESET)**
+    *   
+    *   When the RESET bit is set to one, all configuration, threshold, and data registers are reset
+    *   to their power-on-state through a power-on reset. The RESET bit is cleared automatically back 
+    *   to zero after the reset sequence is completed. 
+    *   Note: Setting the RESET bit does not trigger a PWR_RDY interrupt event.
+    *
+    *   **Bits 2:0: Mode Control**
+    *
+    *   These bits set the operating state of the MAX30101. Changing modes does not change any other setting,
+    *   nor does it erase any previously stored data inside the data registers.
+    *
+    *   | MODE[2:0] | MODE | ACTIVE LED CHANNELS |
+    *   |:---------:|:------:|:--------:|
+    *   |   000     |    <td colspan=2> Do not use   |
+    *   |   001     |    <td colspan=2> Do not use   |    
+    *   |   010     |    Heart Rate mode   |    Red only    |     
+    *   |   011     |    Sp02 mode   |    Red and IR   |     
+    *   |   100-110 |  <td colspan=2> Do not use   |  
+    *   |   111     |   Multi-LED mode   |    Green, RED, and/or IR
+    */
+    #define MAX30101_MODE_CONF 0x09
+    
+    /**
+    *   \brief Sp02 Configuration register.
+    *
+    *   This register configures the sensor for SpO2 mode.
+    *
+    *   It is structured as follows:
+    *
+    *    |   B7   |   B6   |    B5  |   B4   |   B3   |   B2   |   B1   |   B0   |
+    *    | ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ | 
+    *    | |<td colspan=2>SPO2_ADC_RGE[1:0] | <td colspan=3> SPO2_SR[2:0] | <td colspan=2> LED_PW[1:0]|
+    */
+    #define MAX30101_SP02_CONF 0x0A
+    
+    //#define MAX30101_LED1_PA 0x0C
+    
     
 #endif
 
